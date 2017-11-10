2b707a714dd4d07d88165f11ce2659f4360d59f0
sid374 <bidasar2@illinois.edu>
2017-11-09 22:46:29 -0500

    sending email complete

--- checkiPhoneAvailability.py
+++ checkiPhoneAvailability.py
@@ -15,7 +15,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def checkAvailibilityForModel(modelCode='MQAQ2LL/A', zipCode = '33308'):
+def checkAvailibilityForModel(modelCode='MQAQ2LL/A', zipCode = '33308', description= 'iphone' ):
 	''' Checks the in store pick up availability for the modelcode and zipcode provided'''
 	availability_url = APPLE_BASE_URL_STORE_AVAILABILITY + modelCode + APPLE_URL_LOCATION_EXTENSION + zipCode
 	r = requests.get(availability_url)
@@ -26,18 +26,42 @@ def checkAvailibilityForModel(modelCode='MQAQ2LL/A', zipCode = '33308'):
 	jsonData = r.json()
 	for store in jsonData['body']['stores']:
 		#print store['storeName']
-		if store['partsAvailability'][modelCode]['pickupDisplay'] == 'unavailable':
-			logger.log(logging.INFO, store['storeName'] + ': unavailable')
+		if store['partsAvailability'][modelCode]['pickupDisplay'] == 'unavailable' or store['partsAvailability'][modelCode]['pickupSearchQuote'] == 'Currently unavailable':
+			logger.log(logging.INFO, store['storeName'] + ': ' + description + ' unavailable' + ' Zip: ' + zipCode)
 		else:
-			logger.log(logging.INFO, store['storeName'] + ': Available!')
+			logger.log(logging.INFO, store['storeName'] + ': ' + description + ' AVAILABLE!' + ' Zip: ' + zipCode)
+			logger.log(logging.DEBUG, store)
+			sendEmail("Phone {0} available at {1} {2}".format(description, store['storeName'], zipCode))
+
+
+
+def sendEmail(content='Test'):
+	try:
+		yag = yagmail.SMTP('sidautoemail')
+		yag.send(to = 'sid.bidasaria@gmail.com', contents = content)
+	except:
+		e = sys.exc_info()[0]
+		logger.error('Sending email failed')
+
+
+
+def checkAvailability():
+	SPACEGRAY_64 = {'modelCode':'MQAQ2LL/A', 'description':'SPACE GRAY 64'}
+	SILVER_64 = {'modelCode':'MQAR2LL/A', 'description':'SILVER 64'}
+	SPACEGRAY_256 = {'modelCode':'MQAU2LL/A', 'description':'SPACE GRAY 256'}
+	SILVER_256 = {'modelCode' : 'MQAV2LL/A', 'description':'SILVER 256'}
+
+	modelList = [SPACEGRAY_64, SILVER_64, SPACEGRAY_256, SILVER_256]
+	zipcodeList = ['33308', '94116', '98112']
+
+	for model in modelList:
+		for zipcode in zipcodeList:
+			checkAvailibilityForModel(zipCode = zipcode, **model)
+
 
 			
-def sendEmail():
-	yag = yagmail.SMTP('sidautoemail')
-	yag.send(to = 'sid.bidasaria@gmail.com', contents = 'Test')
-			
 def main():
-	#checkAvailibilityForModel()
-	sendEmail()
+	checkAvailability()
+
 if __name__ == "__main__":
     main()
